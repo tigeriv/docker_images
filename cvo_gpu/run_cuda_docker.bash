b8ad25c25fe51207e8c152d75c35dfd532b5ce4f
zeroAska <trotwoodray@gmail.com>
2021-05-11 22:54:46 -0400

    update run docker bash

--- cvo_gpu/run_cuda_docker.bash
+++ cvo_gpu/run_cuda_docker.bash
@@ -1,18 +1,14 @@
 container_name=$1
 
 xhost +local:
-docker run -it --net=host --gpus all  \               # this enable nvidia driver
-  -e DISPLAY=${DISPLAY} \
+docker run -it --net=host --gpus all  -e DISPLAY=${DISPLAY} \
   -e QT_GRAPHICSSYSTEM=native \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
   -e XAUTHORITY \
-  -e USER=${USER} \                                     # use the same username as you
-  --workdir=/home/$USER/ \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \           
-  -v "/etc/passwd:/etc/passwd:rw" \        
-  -e "TERM=xterm-256color" \
-  --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidia-modeset --device /dev/nvidiactl --device /dev/nvidia-uvm-tools \
-  -v "/home/$USER/code/docker_home:/home/$USER/" \
+  -u `id -u`:`id -g` --workdir="/home/$USER/" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"  -v "/etc/passwd:/etc/passwd:rw"  -e "TERM=xterm-256color" \
+  --device /dev/nvidia0 --device /dev/nvidia-modeset --device /dev/nvidiactl  \
+  -v "/home/$USER/code/docker_home/cvo/:/home/$USER/" \
   -v "/run/media/$USER/Samsung_T5/:/home/$USER/media/Samsung_T5" \
   -v "/run/media/$USER/Samsung_T5_2/:/home/$USER/media/Samsung_T5_2" --name=${container_name}  umrobotics/cvo:latest
   #--user=$(id -u) \
